9ae8fa914eac41c84b6244350495e08eaf104e53
ravi <ravi20apr@gmail.com>
2021-09-28 21:00:13 +0530

    basic version of GPIO

---
+++ main.d
@@ -0,0 +1,4 @@
+.\main.o: C:\Users\Ravi\Desktop\srtm32\main.c
+.\main.o: C:\Users\Ravi\Desktop\srtm32\stm32f103xx.h
+.\main.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\main.o: C:\Users\Ravi\Desktop\srtm32\stm32f103xx_gpio_driver.h
---
+++ stm32f103xx_gpio_driver.d
@@ -0,0 +1,4 @@
+.\stm32f103xx_gpio_driver.o: C:\Users\Ravi\Desktop\srtm32\stm32f103xx_gpio_driver.c
+.\stm32f103xx_gpio_driver.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
+.\stm32f103xx_gpio_driver.o: C:\Users\Ravi\Desktop\srtm32\stm32f103xx_gpio_driver.h
+.\stm32f103xx_gpio_driver.o: C:\Users\Ravi\Desktop\srtm32\stm32f103xx.h
